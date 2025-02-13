f3131d599ce08244b3e122b06eaf44e237fe33cb
ujjwal <uzzwal7505@gmail.com>
2025-02-13 15:13:08 +0530

    fixes of error

--- src/Landing page/Comparisonsec.jsx
+++ src/Landing page/Comparisonsec.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import CircleGraph from "./Circlegraph";
+import CircleGraph from "/src/Landing page/CircleGraph.jsx";
 const stats = [
   {
     number: "+300",
