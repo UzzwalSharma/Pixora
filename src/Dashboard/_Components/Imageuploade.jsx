6fb9733fb7405de351d3091e261c13d2c74ee43c
ujjwal <uzzwal7505@gmail.com>
2025-03-27 12:54:24 +0530

    new models

--- src/Dashboard/_Components/Imageuploade.jsx
+++ src/Dashboard/_Components/Imageuploade.jsx
@@ -15,7 +15,7 @@ const aiModels = [
   { name: "Gemini [Recommended]", img: "/Google_Bard_logo.svg - Copy.png", model_name: "google/gemini-2.0-flash-thinking-exp-1219:free" },
   { name: "Deepseek", img: "/deepseek.png", model_name: "google/gemini-2.5-pro-exp-03-25:free" },
   { name: "Meta LLM", img: "/meta.png", model_name: "google/gemini-2.0-flash-thinking-exp:free" },
-  { name: "Gemini 2.0 PRO", img: "/Pro gemini.jpg", model_name: "google/gemini-2.0-pro-exp-02-05:free" },
+  { name: "Gemini 2.0 PRO", img: "/Pro gemini.jpg", model_name: "google/gemini-2.5-pro-exp-03-25:free" },
 ];
 
 function ImageUpload() {
