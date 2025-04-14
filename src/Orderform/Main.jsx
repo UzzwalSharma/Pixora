3ca7787f2a59dd3af0a415b3b2430ba7cecbc125
ujjwal <uzzwal7505@gmail.com>
2025-04-15 00:41:16 +0530

    fix

--- src/Orderform/Main.jsx
+++ src/Orderform/Main.jsx
@@ -3,7 +3,7 @@ import Hero from "/src/Orderform/Hero.jsx";
 import WhyChooseUs from "./Whychooseus";
 import FullstackForm from "./Form";
 import Footer from "/src/Orderform/Footer.jsx"
-import Doubt from "/src/Orderform/doubt.jsx"
+import Doubt from "/src/Orderform/Doubt.jsx"
 function Main() {
   return (
     <div>
