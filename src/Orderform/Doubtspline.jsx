c380f4c5bfae796894a00fb5747d030525986390
ujjwal <uzzwal7505@gmail.com>
2025-04-16 07:19:49 +0530

    new spline

--- src/Orderform/Doubtspline.jsx
+++ src/Orderform/Doubtspline.jsx
@@ -1,11 +1,9 @@
-import Spline from '@splinetool/react-spline';
+import Spline from "@splinetool/react-spline";
 
 export default function DoubtSpline() {
   return (
     <div>
-      <Spline scene="https://prod.spline.design/WctobcTReclqOuag/scene.splinecode" />
-  
- 
+      <Spline scene="https://prod.spline.design/8VCNqGJp-Qfp4LxY/scene.splinecode" />
     </div>
   );
 }
